285c2acafcf41855620ed2d7395864cb98c8817b
Thisal Senevirathne <salpurathisal@gmail.com>
2026-01-18 22:51:48 +0530

    Optimize the home page code and css file ( changed colors )

--- frontend/worky/app/page.tsx
+++ frontend/worky/app/page.tsx
@@ -9,10 +9,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function Home() {
   return (
     <div className="flex flex-col h-full items-center justify-center gap-10">
-      <section className="w-full h-auto bg-custom-white-transparent border border-white backdrop:blur-2xl rounded-lg">
+      <section className="w-full h-auto bg-custom-light-black border border-black rounded-lg">
         <div className="grid grid-cols-12 grid-rows-12 h-full">
           <div className="col-start-1 col-span-12 md:col-span-4 row-start-1 row-span-3 md:row-span-9 flex items-center md:items-start p-5">
-            <p className="text-2xl md:text-xl lg:text-3xl text-center md:text-left font-heading custom-text-style custom-text-style-black"><span className="custom-text-style-pink">Find skilled freelancers ready to bring your ideas to life.</span> Post your project, connect with talent, and get quality work done—on your terms.</p>
+            <p className="text-2xl md:text-xl lg:text-3xl text-center md:text-left font-heading custom-text-style custom-text-style-white"><span className="custom-text-style-pink">Find skilled freelancers ready to bring your ideas to life.</span> Post your project, connect with talent, and get quality work done—on your terms.</p>
           </div>
 
           <div className="col-start-1 md:col-start-5 col-span-12 md:col-span-4 row-start-4 md:row-start-3 row-span-6 md:row-span-8 flex items-center justify-center overflow-hidden p-5">
@@ -27,7 +27,7 @@ export default function Home() {
           </div>
 
           <div className="col-start-1 md:col-start-9 col-span-12 md:col-span-4 row-start-10 md:row-start-4 row-span-3 md:row-span-9 flex items-center md:items-end p-5">
-            <p className="text-2xl md:text-xl lg:text-3xl text-center md:text-right font-heading custom-text-style custom-text-style-black">Turn your skills into income. <span className="custom-text-style-purple">Create your profile, showcase your expertise, and start getting hired by clients who value what you do.</span></p>
+            <p className="text-2xl md:text-xl lg:text-3xl text-center md:text-right font-heading custom-text-style custom-text-style-white">Turn your skills into income. <span className="custom-text-style-purple">Create your profile, showcase your expertise, and start getting hired by clients who value what you do.</span></p>
           </div>
         </div>
       </section>
@@ -44,18 +44,27 @@ export default function Home() {
             }}
             pagination={{ clickable: true }}
             modules={[Autoplay, Pagination]}
+            breakpoints={{
+              320: { slidesPerView: 1 },
+              768: { slidesPerView: 2 },
+              1024: { slidesPerView: 3 },
+            }}
             className="w-full h-auto"
           >
             {categories.map((category) => (
-              <SwiperSlide key={category.id} className="flex! flex-col items-center justify-center bg-custom-white-transparent border border-white backdrop:blur-2xl rounded-lg p-10 gap-10">
-                <FontAwesomeIcon icon={category.icon} className="text-7xl lg:text-9xl text-center" />
-                <p className="mb-2.5 text-xl lg:text-3xl text-center font-heading custom-text-style custom-text-style-black">
+              <SwiperSlide key={category.id} className="flex! flex-col items-center justify-center bg-custom-light-black border border-black rounded-lg p-10 gap-10">
+                <FontAwesomeIcon icon={category.icon} className="text-7xl lg:text-9xl text-custom-white text-center" />
+                <p className="mb-2.5 text-xl lg:text-3xl text-center font-heading custom-text-style custom-text-style-white">
                   {category.title}
                 </p>
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
+      </section>
+
+      <section>
+
       </section>
     </div>
   );
