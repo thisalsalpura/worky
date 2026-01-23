438bc0e99c6af09ffebcea29e079794ecec0e6d6
Thisal Senevirathne <salpurathisal@gmail.com>
2026-01-23 21:16:00 +0530

    Optimize the existing code and add "Download App" and "Newsletter" sections

--- frontend/worky/components/Button.tsx
+++ frontend/worky/components/Button.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 
 interface ButtonProps {
   name: string;
+  btn_class?: string;
   front_classes: string;
   back_classes: string;
   text_color?: string;
-  fullWidth?: boolean;
 }
 
-const Button = ({ name, front_classes, back_classes, text_color, fullWidth = false }: ButtonProps) => {
+const Button = ({ name, btn_class = '', front_classes, back_classes, text_color = '' }: ButtonProps) => {
   return (
-    <button className={`cus-btn-con ${fullWidth ? 'w-full' : 'w-auto'}`}>
+    <button className={`${btn_class} cus-btn-con`}>
       <div className={`${front_classes || ''} cus-btn-front clip-custom`}>
         <p className={`${text_color} text-lg text-center font-body font-semibold`}>{name}</p>
       </div>
