6bc227cdfbd22504a535e1ce176ec76688222a78
Thisal Senevirathne <salpurathisal@gmail.com>
2026-04-28 12:03:12 +0530

    Add IntelliJ project files and update .gitignore
    Add a set of .idea project configuration files and a HELP.md for the backend; expand backend and frontend .gitignore files to cover IntelliJ, Maven, Node/Next, VS Code and dotenv/local env files. Also include small whitespace/formatting tweaks in mvnw, mvnw.cmd, pom.xml and minor indentation adjustments in the main application and test Java files.

--- backend/worky/src/test/java/com/thisal/worky/WorkyApplicationTests.java
+++ backend/worky/src/test/java/com/thisal/worky/WorkyApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WorkyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
